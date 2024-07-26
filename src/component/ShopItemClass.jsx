77ec922f0f6bf14f12e57af88f665c7d5ae10e0a
Kelias1 <kelsito24@gmail.com>
2024-07-26 07:27:03 +0300

    lint modified

--- src/component/ShopItemClass.jsx
+++ src/component/ShopItemClass.jsx
@@ -1,23 +1,25 @@
+/* eslint-disable react/prop-types */
+
 import { Component } from  "react";
 
 export class ShopItemClass extends Component {
 
 	render() {
-
+		
 		const {brand, title, description, descriptionFull, price, currency} = this.props.item;
 
 		return (
-			<div class="main-content">
+			<div className="main-content">
 				<h2>{brand}</h2>
 				<h1>{title}</h1>
 				<h3>{description}</h3>
-				<div class="description">
+				<div className="description">
 					{descriptionFull}
 				</div>
-				<div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-				<div class="divider"></div>
-				<div class="purchase-info">
-					<div class="price">{currency + price.toFixed(2)}</div>
+				<div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+				<div className="divider"></div>
+				<div className="purchase-info">
+					<div className="price">{currency + price.toFixed(2)}</div>
 					<button>Добавить в корзину</button>
 				</div>
 			</div>
